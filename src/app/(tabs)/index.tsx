dd75e9bfa01d77ed6f413ff15da257e96b2cf6f7
Khant Zaw Phyo <khantzawphyo02@gmail.com>
2025-10-14 17:33:16 +0630

    Setup NativeWind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,17 +2,9 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Feed</Text>
+    <View>
+      <Text className='mx-auto text-3xl font-bold text-red-300'>Feed</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
